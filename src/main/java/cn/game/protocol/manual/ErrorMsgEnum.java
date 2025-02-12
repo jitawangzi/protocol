bb95257efe76fe68e7da8acddd6a04071e451d5c
liuyang_cp <mengqing20101212@163.com>
2025-02-12 14:53:06 +0800

    宗门活跃度领取修改

--- src/main/java/cn/game/protocol/manual/ErrorMsgEnum.java
+++ src/main/java/cn/game/protocol/manual/ErrorMsgEnum.java
@@ -169,7 +169,7 @@ public enum ErrorMsgEnum {
 	zong_men_apply_join_timer(419, "宗门申请加入冷却时间未到，不可申请"),
 	zong_men_player_member_not_exist(420, "宗门成员不存在"),
 	zong_men_position_member_num_not_enough(421, "宗门职位成员数量不足，不可操作"),
-
+	zong_men_active_reward_already_get(422, "宗门活跃度奖励已经领取过了"),
 
 
 	max_error_id(999, "占位错误码");
