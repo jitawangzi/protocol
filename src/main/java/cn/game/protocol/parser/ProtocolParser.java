fd2acc50b2bb3ee7b1cd4408a05cfdbef1521621
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-06-24 16:56:25 +0800

    协议解析

--- src/main/java/cn/game/protocol/parser/ProtocolParser.java
+++ src/main/java/cn/game/protocol/parser/ProtocolParser.java
@@ -1,8 +1,12 @@
 package cn.game.protocol.parser;
 
+import java.nio.ByteBuffer;
+
 public interface ProtocolParser {
 
 	public Object parseFrom(int msgID, byte[] data);
+
+	public Object parseFrom(int msgID, ByteBuffer data);
 
 	public int getMsgId(String name);
 
