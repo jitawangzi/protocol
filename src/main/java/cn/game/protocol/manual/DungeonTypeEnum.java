31afefa50045cd18b6426b411251e18ef5e4c005
gh_quyukun <email@sample.com>
2025-08-01 17:20:49 +0800

    【程序自用任务】大圣来巡山 服务器开发 PA202508012139

--- src/main/java/cn/game/protocol/manual/DungeonTypeEnum.java
+++ src/main/java/cn/game/protocol/manual/DungeonTypeEnum.java
@@ -43,7 +43,7 @@ public enum DungeonTypeEnum{
 	BattleEvent(33, "突发事件"),
 	/** 探索战斗 */
 	ExploreBattle(44, "探索战斗"),
-
+	MountainBattle(35, "大圣来巡山"),
 	// 合成游戏的战斗
 	HCBattleChapter(11, "合成游戏主线章节"),
 	DayChallenge(12, "合成每日挑战"),
