8acd17e9db54add635f84b48edbe1a255bd8226f
gh_quyukun <email@sample.com>
2025-10-10 11:17:44 +0800

    【功能开发】通行证功能调整-服务器 PA202509241312-1-2

--- src/main/java/cn/game/protocol/manual/OpType.java
+++ src/main/java/cn/game/protocol/manual/OpType.java
@@ -204,6 +204,7 @@ public enum OpType{
 	MountainRefresh(302, "大圣来巡山刷新"),
 	MountainPointReward(303, "大圣来巡山积分奖励"),
 	PVEVPBattleQuickEnd(304, "PVEVP战斗快速结束"),
+	FundPassSignActivity(305, "登录通行证活动"),
 	PressureTest(888, "压力测试"),
 	None(999, "未定义"),;
 
